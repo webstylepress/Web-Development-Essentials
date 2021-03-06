eb0b4c935b07660e113dea5efcac84e419ffd239
webstylepress <info@webstylepress.com>
2021-03-07 01:17:26 +0500

    update

--- php/embed.php
+++ php/embed.php
@@ -1,7 +1,9 @@
 <?php 
+
   $videoURL = "https://www.youtube.com/watch?v=0iAUo8W-nDQ";
   $convertedURL = str_replace("watch?v=","embed/", $videoURL);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -14,17 +16,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
   </head>
   <body>
+
     <br><br>
+
     <section>
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
+            
             <div class="embed-responsive embed-responsive-16by9">
               <iframe class="embed-responsive-item" src="<?php echo $convertedURL; ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
+
           </div>
         </div>
       </div>    
     </section>
+
   </body>
 </html>
